38fa952ac0cbd42eff41db0e72921cd6236969e0
pedrofp4444 <pfpereira2004@gmail.com>
2023-01-24 15:33:29 +0000

    Add SliceLists

---
+++ Starter programs/SliceLists.py
@@ -0,0 +1,33 @@
+# Python Program slice lists
+
+my_list = [1, 2, 3, 4, 5]
+
+# Get all the Items
+
+print(my_list[:])
+
+# Get all the Items After a Specific Position
+
+print(my_list[2:])
+
+# Get all the Items Before a Specific Position
+
+print(my_list[:2])
+
+# Get all the Items from One Position to Another Position
+
+print(my_list[2:4])
+
+# Get the Items at Specified Intervals
+
+# Option 1
+
+print(my_list[::2])
+
+# Option 2
+
+print(my_list[::-2])
+
+# Option 3
+
+print(my_list[1:4:2])
